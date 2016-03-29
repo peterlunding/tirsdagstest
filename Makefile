a561f6693e45808919aff4038512430652bc7582
Jakob Bæk Tejs Knudsen <jakobtejs@gmail.com>
2016-03-29 22:19:22 +0200

    Compile visual studio

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	g++ hello.cpp -std=c++11 -o app
+
+run:
+	app
--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main() {
   cout << test() << endl;
+  cout << 2*test() << endl;
   cout << "Hello world!" << endl;
   return 0;
 }
