87697680ad41592cf75c83a62dfd82fd34da10ad
Jakob Bæk Tejs Knudsen <jakobtejs@gmail.com>
2016-03-29 21:29:38 +0200

    Simple app

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "world.cpp"
+
+using namespace std;
+
+int main() {
+  cout << test() << endl;
+  cout << "Hello world!" << endl;
+  return 0;
+}
